63ce971cc6b81698f23432fe249ffa018726d594
jpuri <jyotipuri@gmail.com>
2019-09-09 22:11:26 +0530

    Script update

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -11,7 +11,7 @@ git add styleguide
 git commit -am 'deploying docs'
 
 git subtree split --prefix styleguide -b gh-pages
-git push -f deploy gh-pages:gh-pages
+git push -f origin gh-pages:gh-pages
 git checkout master
 git branch -D tmp-gh-pages
 git branch -D gh-pages
