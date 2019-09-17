c2b56d7a58b833699a861fb1fb31d85f995dd01c
jpuri <jyotipuri@gmail.com>
2019-09-17 14:26:24 +0530

    Collab demo improvements

--- packages/docs/demo/Collab/index.jsx
+++ packages/docs/demo/Collab/index.jsx
@@ -3,18 +3,16 @@ import Editor from "nib-core";
 
 import CollabPlugin from "nib-collab-client";
 
+const collab = new CollabPlugin({
+  serviceURL: "ws://nib-collab.herokuapp.com",
+  clientID: Math.floor(Math.random() * 0xffffffff)
+})
+
 /**
  * @visibleName 11. Collaborative Editing
  */
 const Collab = () => {
   const [editorState, setEditorState] = useState();
-  const [collab] = useState(
-    () =>
-      new CollabPlugin({
-        serviceURL: "ws://nib-collab.herokuapp.com",
-        clientID: Math.floor(Math.random() * 0xffffffff)
-      }) // https://nib-collab.herokuapp.com
-  );
 
   useEffect(() => {
     return () => {
