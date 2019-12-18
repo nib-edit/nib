ba4b1b213f72fae663432d8ba9a1920b6518518e
jpuri <jyotipuri@gmail.com>
2019-12-18 17:46:49 +0530

    docs improvements

--- packages/docs/demo/Track/index.jsx
+++ packages/docs/demo/Track/index.jsx
@@ -1,24 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import Editor from "nib-core";
-import TrackPlugin from "nib-track";
+import Editor from 'nib-core';
+import TrackPlugin from 'nib-track';
 
-import defaultValue from "./sampleData";
-import commits from "./savedCommits";
+import defaultValue from './sampleData';
+import commits from './savedCommits';
 
-import "./styles.css";
+import './styles.css';
 
 const tracker = new TrackPlugin(commits);
 
 const userid = Math.floor(Math.random() * 0xffffffff);
+
+const formatDate = str => {
+  const d = new Date(str);
+  if (!d) return '';
+  return `${d.getMonth() +
+    1}-${d.getDate()}-${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+};
 
 /**
  * @visibleName 17. Track Changes
  */
 const Track = () => {
   const [trackState, setTrackState] = useState(tracker.getState());
-  const [name, setName] = useState("Anonymous user");
-  const [cmd, setCmd] = useState("Anonymous user");
+  const [name, setName] = useState('Anonymous user');
+  const [cmd, setCmd] = useState('Anonymous user');
 
   const updateTrackedState = () => {
     setTrackState(tracker.getState());
@@ -40,8 +47,8 @@ const Track = () => {
     updateTrackedState();
   };
   const onKeyDown = evt => {
-    if (evt.key === "Meta") setCmd(true);
-    else if (evt.key === "s" && cmd) {
+    if (evt.key === 'Meta') setCmd(true);
+    else if (evt.key === 's' && cmd) {
       doCommit();
       evt.preventDefault();
     }
@@ -61,62 +68,70 @@ const Track = () => {
         placeholder="Enter user name"
         value={name}
       />
-      <div onKeyDown={onKeyDown} onKeyUp={onKeyUp}>
-        <Editor
-          config={{
-            plugins: { options: "block inline list" },
-            toolbar: { options: "top", top: { options: "block inline list" } }
-          }}
-          addons={[tracker]}
-          defaultValue={defaultValue}
-          onChange={updateTrackedState}
-        />
+      <div className="nib-track-wrapper">
+        <div>
+          <div onKeyDown={onKeyDown} onKeyUp={onKeyUp}>
+            <Editor
+              config={{
+                plugins: { options: 'block inline list' },
+                toolbar: {
+                  options: 'top',
+                  top: { options: 'block inline list' },
+                },
+              }}
+              addons={[tracker]}
+              defaultValue={defaultValue}
+              onChange={updateTrackedState}
+            />
+          </div>
+          <div className="nib-track_save_wraper">
+            <button
+              className="nib-track_save"
+              disabled={!trackState.hasUncommittedSteps}
+              onClick={doCommit}
+              type="button"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+        <div>
+          <div className="nib-track_commits">Changes</div>
+          <table className="nib-track_table">
+            <tbody>
+              {trackState.commits.map((commit, index) => (
+                <tr key={`${commit.time}`}>
+                  <td>
+                    <div
+                      className={
+                        trackState.highlightedCommit &&
+                        trackState.highlightedCommit === commit
+                          ? 'nib-highlighted-commit'
+                          : ''
+                      }
+                      onMouseEnter={() => highlightCommit(commit)}
+                      onMouseLeave={resetHighlight}
+                    >
+                      <div>{`${index + 1}. ${commit.data.username}`}</div>
+                      <div>{formatDate(commit.time)}</div>
+                      <div>
+                        <button
+                          className="nib-track_revert_btn"
+                          onClick={() => revertCommit(commit)}
+                          type="button"
+                        >
+                          Revert
+                        </button>
+                      </div>
+                    </div>
+                    <div className="nib-track-space" />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-      <div className="nib-track_save_wraper">
-        <button
-          className="nib-track_save"
-          disabled={!trackState.hasUncommittedSteps}
-          onClick={doCommit}
-          type="button"
-        >
-          Save
-        </button>
-      </div>
-      <div className="nib-track_commits">Changes</div>
-      <table className="nib-track_table">
-        <tr>
-          <th>Id</th>
-          <th>User Name</th>
-          <th>Time</th>
-          <th>Revert</th>
-        </tr>
-        {trackState.commits.map(commit => (
-          <tr
-            className={
-              trackState.highlightedCommit &&
-              trackState.highlightedCommit === commit
-                ? "nib-highlighted-commit"
-                : ""
-            }
-            onMouseEnter={() => highlightCommit(commit)}
-            onMouseLeave={resetHighlight}
-            key={`${commit.time}`}
-          >
-            <td>{commit.id + 1}</td>
-            <td>{commit.data.username}</td>
-            <td>{commit.time.toLocaleString()}</td>
-            <td>
-              <button
-                className="nib-track_revert_btn"
-                onClick={() => revertCommit(commit)}
-                type="button"
-              >
-                Revert
-              </button>
-            </td>
-          </tr>
-        ))}
-      </table>
     </div>
   );
 };
