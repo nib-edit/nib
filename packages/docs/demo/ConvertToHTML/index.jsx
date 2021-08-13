e8e8ecd9fba6e10bd04d9074b65ad8ae64b1b063
권신학 <cheatkey33@gmail.com>
2021-08-13 23:01:45 +0900

    fix broken demo convert to HTML

--- packages/docs/demo/ConvertToHTML/index.jsx
+++ packages/docs/demo/ConvertToHTML/index.jsx
@@ -1,18 +1,24 @@
 import Editor from 'nib-core';
 import React, { useState } from 'react';
 import NibConverter from 'nib-converter';
+import { marks, nodes } from "nib-schema";
 
+import { Schema } from "prosemirror-model";
 import Code from '../../Code';
 import defaultValue from './sampleData';
-
 import './styles.css';
+
+const schema = new Schema({
+  nodes,
+  marks,
+});
 
 /**
  * @visibleName 15. Convert to HTML
  */
 const ConvertToHTMLDemo = () => {
   const [content, setContent] = useState();
-  const htmlStr = NibConverter.convertToHTML((content || defaultValue).doc);
+  const htmlStr = NibConverter.convertToHTML(schema, (content || defaultValue).doc);
   return (
     <div>
       <Editor
